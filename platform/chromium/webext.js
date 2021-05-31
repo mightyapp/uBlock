1dc3d184c8d5798fc690e52ed45a1cafd958a05f
@anulman <anulman@gmail.com>
2021-05-31 16:07:21 -0400

    Proxy `chrome.storage.onChanged` through webext, if available

--- platform/chromium/webext.js
+++ platform/chromium/webext.js
@@ -158,6 +158,11 @@ if ( chrome.storage.sync instanceof Object ) {
     };
 }
 
+// https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage/onChanged
+if ( chrome.storage.onChanged instanceof Object ) {
+    webext.storage.onChanged = chrome.storage.onChanged;
+}
+
 // https://bugs.chromium.org/p/chromium/issues/detail?id=608854
 if ( chrome.tabs.removeCSS instanceof Function ) {
     webext.tabs.removeCSS = promisifyNoFail(chrome.tabs, 'removeCSS');
